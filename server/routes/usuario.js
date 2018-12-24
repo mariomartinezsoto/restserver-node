1442c076f62fcb5599eb1866cf76f7f4dde8ea4e
Mario <mariomartinezsoto@gmail.com>
2018-12-24 01:06:04 -0300

    mejoras

---
+++ server/routes/usuario.js
@@ -0,0 +1,136 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const _ = require('underscore')
+const app = express()
+const Usuario = require('../models/usuario')
+
+app.get('/usuario', function(req, res) {
+    let desde = Number(req.query.desde) || 0
+    let limite = Number(req.query.limite) || 5
+
+    Usuario.find({ estado: true }, 'nombre email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            Usuario.count({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    registrosTotales: conteo,
+                    usuarios
+                })
+            })
+        })
+})
+
+app.post('/usuario', function(req, res) {
+    let body = req.body
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    })
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+    })
+})
+
+app.put('/usuario/:id', function(req, res) {
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado'])
+    let id = req.params.id
+
+    Usuario.findByIdAndUpdate(id, body, {
+        new: true,
+        runValidators: true
+    }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+    })
+})
+
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id
+    let cambiaEstado = {
+        estado: false
+    }
+
+    //BORRADO LOGICO
+    //------------------------------------------------------------
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            })
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+    })
+
+    // BORRADO FISICO
+    //------------------------------------------------------------
+    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    //     if (err) {
+    //         return res.status(400).json({
+    //             ok: false,
+    //             err
+    //         })
+    //     }
+
+    //     if (usuarioBorrado == null) {
+    //         return res.status(400).json({
+    //             ok: false,
+    //             err: {
+    //                 message: 'Usuario no encontrado'
+    //             }
+    //         })
+    //     }
+
+    //     res.json({
+    //         ok: true,
+    //         usuario: usuarioBorrado
+    //     })
+    // })
+
+})
+
+module.exports = app
--- server/server.js
+++ server/server.js
@@ -1,42 +1,16 @@
 require('./config/config')
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(require('./routes/usuario'))
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario LOCAL')
-})
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-})
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id
-
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if (err) throw err
+    console.log('Base de datos online');
 })
 
 app.listen(process.env.PORT, () => {
