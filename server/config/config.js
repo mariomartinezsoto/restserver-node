68837a93cb311b0f0e2a29d0be87358e902e06c1
Mario <mariomartinezsoto@gmail.com>
2018-12-30 23:42:40 -0300

    Google sign in

--- server/config/config.js
+++ server/config/config.js
@@ -16,3 +16,6 @@ process.env.CADUCIDAD_TOKEN = '30d'
 
 //SEED de autenticacion
 process.env.SEED = process.env.SEED || 'seed-de-desarrollo'
+
+//GOOGLE
+process.env.CLIENT_ID = process.env.CLIENT_ID || '995458413462-aep6569is8favjdtv3lafcrjlcc5u833.apps.googleusercontent.com'
--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 require('./config/config')
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const bodyParser = require('body-parser')
 const colors = require('colors')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(express.static(path.resolve(__dirname,'../public')))
 app.use(require('./routes/index'))
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
